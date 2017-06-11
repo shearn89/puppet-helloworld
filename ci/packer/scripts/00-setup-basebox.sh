c43c64226ae82d88fe1b90e746fc74be150e6b6a
shearna <alexander.shearn@gmail.com>
2017-06-11 15:54:10 +0100

    Adding Packer files for Part 3 of the blog

---
+++ ci/packer/scripts/00-setup-basebox.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+curl -L -O https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
+mkdir -m 0700 .ssh
+mv vagrant.pub .ssh
+cat .ssh/*.pub > .ssh/authorized_keys
+chmod 600 .ssh/*
+
+sudo sed -i '/UseDNS/ s/yes/no/' /etc/ssh/sshd_config
+sudo sed -i 's/#UseDNS/UseDNS/' /etc/ssh/sshd_config
+
+sudo mkdir -p /media/cdrom
+sudo mount /dev/sr1 /media/cdrom
+sudo ls -l /media/cdrom
+sudo sh /media/cdrom/VBoxLinuxAdditions.run
+sudo umount /media/cdrom
---
+++ ci/packer/scripts/01-install-puppet.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+
+sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+sudo yum install -y puppet
